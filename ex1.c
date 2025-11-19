0c6f5b95ba9823930f89c2c5f80336e30980d672
lollo <Tawfeek.wattad@gmail.com>
2025-11-19 16:10:13 +0200

    ex1

--- ex1.c
+++ ex1.c
@@ -9,15 +9,17 @@ Assignment: ex1
 
 int main()
 {
-    printf("Ascii:\n PLease enter a character:\n");
+    //1
+    printf("Ascii:\nPlease enter a character\n");
     char ch;
     scanf("%c", &ch);
-    printf("It's numerical value is : %d\n",ch);
+    printf("Its numerical value is: %d\n",ch);
     int num,tem;
     num =ch;
     tem = num%2;
     printf("0 for even, 1 for odd: %d\n\n\n\n\n\n" ,tem);
 
+    //2
     printf("2's complement to other representations: \nPlease enter a negative integer\n");
     int x,com;
     scanf("%d",&com);
@@ -26,7 +28,7 @@ int main()
     printf("1's complement: -%d\nunsigned: %u\n\n\n\n\n\n",x,uns);
 
 
-
+    //3
     printf("Shifting right and left: \nPlease enter 3 integers\n");
     int a,b,c;
     scanf("%d%d%d",&a,&b,&c);
@@ -35,7 +37,7 @@ int main()
     printf("After shifting right and left: %d\n\n\n\n\n\n",a);
 
 
-
+    //4
     printf("Even - Odd:\nPlease enter 3 integers\n");
     int d,e,f,sum;
     scanf("%d%d%d", &d,&e,&f);
@@ -44,8 +46,8 @@ int main()
     f = f%2;
     sum = d+e+f;
     sum= sum/2;
-    printf("0 - most of them are even,1 - most of them are odd: %d\n\n\n\n\n\n",sum);
-
+    printf("0 - most of them are even, 1 - most of them are odd: %d\n\n\n\n\n\n",sum);
+    //5
     printf("Different Bases:\nPlease enter two numbers in octal and hexadecimal bases\n");
 int oc,hx,lsbs,msbs;
 scanf("%o%x",&oc,&hx);
